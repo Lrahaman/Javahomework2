b53ecb2ba100924c2cf183dee344dacd82ec059b
nessa <lutforny@gmail.com>
2023-02-21 11:08:07 -0500

    comitted

--- Javahomework 2/src/Assignment2/AddOddNumbers.java
+++ Javahomework 2/src/Assignment2/AddOddNumbers.java
@@ -3,7 +3,9 @@ package Assignment2;
 public class AddOddNumbers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a=10;
+		int y=20;
+		System.out.println(a+y);
 
 	}
 
